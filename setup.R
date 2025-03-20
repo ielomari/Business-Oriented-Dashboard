81182a18cfc674d435d66445b3b8c956d974c46f
Ibtissam <ibtissam.elomari@student.dlh.lu>
2025-03-20 10:15:38 +0000

    libraries for the app

---
+++ setup.R
@@ -0,0 +1,17 @@
+if (!require("reactable")) install.packages("reactable")
+if (!require("DT")) install.packages("DT")
+if (!require("leaflet")) install.packages("leaflet")
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("plotly")) install.packages("plotly")
+if (!require("lubridate")) install.packages("lubridate")
+if (!require("highcharter")) install.packages("highcharter")
+
+
+
+library(reactable) 
+library(DT)  
+library(leaflet)
+library(dplyr)
+library(plotly)
+library(lubridate)
+library(highcharter)
