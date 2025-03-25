0699adb13b900ea46639bdc0877916c3a128d2fa
Ibtissam <ibtissam.elomari@student.dlh.lu>
2025-03-25 12:29:46 +0000

    DT table update

--- layouts/Interactive_transaction_table.R
+++ layouts/Interactive_transaction_table.R
@@ -1,27 +1,9 @@
-# library(DT)  
-# library(dplyr)  
-source("setup.R")
+library(DT)
 
-# Load Data
-retail_data <- read.csv("data/online_retail_combined.csv", stringsAsFactors = FALSE)
-
-# Select only relevant columns
-transactions_data <- retail_data %>%
-  select(Invoice, StockCode, Description, Quantity, InvoiceDate, Price, Customer.ID, Country) %>%
-  mutate(InvoiceDate = as.POSIXct(InvoiceDate, format="%Y-%m-%d %H:%M:%S", tz="UTC"))
-
-# Create interactive DataTable
-get_transactions_table <- function() {
+get_transactions_table <- function(data) {
   datatable(
-    transactions_data,
-    options = list(
-      pageLength = 10,  
-      autoWidth = TRUE, 
-      searchHighlight = TRUE,  
-      dom = 'Bfrtip',  # Show buttons for CSV export
-      buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-    ),
-    extensions = 'Buttons',
+    data %>% select(InvoiceNo, StockCode, Description, Quantity, InvoiceDate, Price, CustomerID, Country),
+    options = list(pageLength = 10),
     rownames = FALSE
   )
 }
