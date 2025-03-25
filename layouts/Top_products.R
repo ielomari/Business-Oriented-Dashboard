080cdfecb21717bb6a1f54209e6d022501171ce7
Ibtissam <ibtissam.elomari@student.dlh.lu>
2025-03-25 12:30:33 +0000

    highcharter update

--- layouts/Top_products.R
+++ layouts/Top_products.R
@@ -1,26 +1,14 @@
-# library(highcharter)
-# library(dplyr)
-source("setup.R")
+library(highcharter)
 
-# Load Data
-retail_data <- read.csv("data/online_retail_combined.csv", stringsAsFactors = FALSE)
-
-# Aggregate Top-Selling Products
-top_products <- retail_data %>%
-  group_by(Description) %>%
-  summarize(TotalQuantity = sum(Quantity, na.rm = TRUE)) %>%
-  arrange(desc(TotalQuantity)) %>%
-  top_n(10, TotalQuantity)  # Select Top 10 Products
-
-# Create Highcharter Bar Chart
-top_products_chart <- highchart() %>%
-  hc_chart(type = "bar") %>%
-  # hc_title(text = "Top 10 Best-Selling Products") %>%
-  hc_xAxis(categories = top_products$Description) %>%
-  hc_yAxis(title = list(text = "Total Quantity Sold")) %>%
-  hc_add_series(name = "Quantity Sold", data = top_products$TotalQuantity)
-
-# Function to return the chart
-get_top_products <- function() {
-  return(top_products_chart)
+get_top_products <- function(data) {
+  top_products <- data %>%
+    group_by(Description) %>%
+    summarize(QuantitySold = sum(Quantity)) %>%
+    arrange(desc(QuantitySold)) %>%
+    slice_max(QuantitySold, n = 10)
+  
+  highchart() %>%
+    hc_chart(type = "bar") %>%
+    hc_xAxis(categories = top_products$Description) %>%
+    hc_add_series(name = "Quantity Sold", data = top_products$QuantitySold)
 }
